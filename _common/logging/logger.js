334695ef4a2bf7c84497cbe3e1699e85c4467f19
Niranjan Godbole <niranjan8192@gmail.com>
2017-05-24 12:48:36 +0530

    trim down logger

--- _common/logging/logger.js
+++ _common/logging/logger.js
@@ -5,10 +5,9 @@ module.exports = self;
 
 var winston = require('winston');
 
-function logger() {
+function logger(logLevel) {
   var logger = winston;
-  // Once systemConfigs are obtained from the API, this will be overwritten.
-  var defaultLogLevel = 'verbose';
+  logLevel = logLevel || 'warn';
 
   logger.clear();
 
@@ -16,72 +15,7 @@ function logger() {
     {
       timestamp: true,
       colorize: true,
-      level: defaultLogLevel
-    }
-  );
-
-  logger.add(winston.transports.File,
-    {
-      name: 'file#out',
-      timestamp: true,
-      colorize: true,
-      filename: util.format('logs/%s.log', global.msName),
-      maxsize: 10 * 1024 * 1024,  // 10 MB
-      maxFiles: 20,
-      level: defaultLogLevel,
-      json: false
-    }
-  );
-
-  logger.add(winston.transports.File,
-    {
-      name: 'file#err',
-      timestamp: true,
-      colorize: true,
-      filename: util.format('logs/%s_err.log', global.msName),
-      maxsize: 10 * 1024 * 1024, // 10 MB
-      maxFiles: 20,
-      level: 'error',
-      json: false
-    }
-  );
-
-  logger.add(winston.transports.File,
-    {
-      name: 'file#warn',
-      timestamp: true,
-      colorize: true,
-      filename: util.format('logs/%s_warn.log', global.msName),
-      maxsize: 5 * 1024 * 1024, // 5 MB
-      maxFiles: 20,
-      level: 'warn',
-      json: false
-    }
-  );
-
-  logger.add(winston.transports.File,
-    {
-      name: 'file#info',
-      timestamp: true,
-      colorize: true,
-      filename: util.format('logs/%s_info.log', global.msName),
-      maxsize: 5 * 1024 * 1024, // 5 MB
-      maxFiles: 20,
-      level: 'info',
-      json: false
-    }
-  );
-
-  logger.add(winston.transports.File,
-    {
-      name: 'file#debug',
-      timestamp: true,
-      colorize: true,
-      filename: util.format('logs/%s_debug.log', global.msName),
-      maxsize: 5 * 1024 * 1024, // 5 MB
-      maxFiles: 20,
-      level: 'debug',
-      json: false
+      level: logLevel
     }
   );
 
--- _common/setupTests.js
+++ _common/setupTests.js
@@ -6,29 +6,18 @@ module.exports = self;
 global.util = require('util');
 global._ = require('underscore');
 global.async = require('async');
+global.logger = require('./logging/logger.js')(process.env.LOG_LEVEL);
 
-var Logger = require('./logging/logger.js');
-
-// each test starts off as a new process
-// this function does necessary setup like getting necessary data
-function setupTests(params) {
-  global.msName = params.msName;
-  process.title = params.msName;
-
-  // setup logger
+// each test starts off as a new process, setup required constants
+function setupTests() {
   global.config = {};
-  global.config.logLevel = 'verbose';
-  global.logger = Logger();
-
-  /* Env Set */
   global.config.apiUrl = process.env.SHIPPABLE_API_URL;
   global.config.apiToken = process.env.SHIPPABLE_API_TOKEN;
 
   global.config.githubUrl = 'https://api.github.com';
-  global.config.githubToken = params.githubToken;
 
   global.configPath = process.env.SHIPPABLE_CONFIG_PATH;
   global.githubOwnerAccessToken = process.env.GITHUB_ACCESS_TOKEN_OWNER;
 
-  // fetch any more data needed for tests below
+  // setup any more data needed for tests below
 }
