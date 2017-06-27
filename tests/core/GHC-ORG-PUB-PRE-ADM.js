3b8bacb9ff300150218fc4f9f0dc4bf5fd22a01e
Niranjan Godbole <niranjan8192@gmail.com>
2017-06-27 19:27:49 +0530

    fix public project name

--- tests/core/GHC-ORG-PUB-PRE-ADM.js
+++ tests/core/GHC-ORG-PUB-PRE-ADM.js
@@ -56,7 +56,7 @@ describe(testSuite + testSuiteDesc,
       logger.debug(who, 'Inside');
 
       // get public project before starting the tests
-      var query = util.format('name=%s', global.GHC_OWNER_PUBLIC_PROJ);
+      var query = util.format('name=%s', global.GHC_PUBLIC_PROJ);
       global.ghcAdminAdapter.getProjects(query,
         function (err, projects) {
           if (err || _.isEmpty(projects))
